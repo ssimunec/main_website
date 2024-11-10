699ae600f5d2f5d8ac2e5d9841cfb34aac51ba2c
ssavicic <savicicns@gmail.com>
2024-11-10 18:01:05 +0100

    feat(3S2): projects and logos udpate (#11)
    Co-authored-by: Sasa Savicic <ssc@emakina.com>

--- js/utilities.js
+++ js/utilities.js
@@ -9,12 +9,21 @@ document.getElementById('scrollToFirst').onclick = function() {
 
 document.getElementById('frontEndButton').onclick = function() { 
     scrollToDiv('frontEndContent');
+    setTimeout(() => {
+        document.getElementById('askMoreFrontEnd').focus();
+    }, 1200);
 };
 
 document.getElementById('architectButton').onclick = function() { 
     scrollToDiv('architectContent');
+    setTimeout(() => {
+        document.getElementById('askMoreArchitecture').focus();
+    }, 1200);
 };
 
 document.getElementById('devopsButton').onclick = function() { 
     scrollToDiv('devopsContent');
+    setTimeout(() => {
+        document.getElementById('askMoreDevOps').focus();
+    }, 1200);
 };
