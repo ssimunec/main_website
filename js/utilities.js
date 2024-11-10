1c7c330ad353608c3a1fc4e59846d674ea2e287c
Sasa Savicic <ssc@emakina.com>
2024-11-10 13:15:26 +0100

    feat(3S2): update scroll to content

--- js/utilities.js
+++ js/utilities.js
@@ -7,6 +7,14 @@ document.getElementById('scrollToFirst').onclick = function() {
     scrollToDiv('firstContent');
 };
 
+document.getElementById('frontEndButton').onclick = function() { 
+    scrollToDiv('frontEndContent');
+};
+
+document.getElementById('architectButton').onclick = function() { 
+    scrollToDiv('architectContent');
+};
+
 document.getElementById('devopsButton').onclick = function() { 
     scrollToDiv('devopsContent');
 };
